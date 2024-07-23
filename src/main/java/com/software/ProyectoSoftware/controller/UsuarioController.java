a0b62d4c2498bebb8017101487cdf6847cdcbd91
Gabriiello <gbrielbeltran22@gmail.com>
2024-07-23 00:07:09 -0500

    Enpoints para el registro User

--- src/main/java/com/software/ProyectoSoftware/controller/UsuarioController.java
+++ src/main/java/com/software/ProyectoSoftware/controller/UsuarioController.java
@@ -3,18 +3,19 @@ package com.software.ProyectoSoftware.controller;
 import com.software.ProyectoSoftware.Models.User;
 import com.software.ProyectoSoftware.dao.daoUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
     @Autowired
     private daoUser daoUser;
 
 
-    @RequestMapping(value="Usuario/login", method= RequestMethod.POST)
+    @RequestMapping(value="Usuario/registro", method= RequestMethod.POST)
     public void agregarUsuario(@RequestBody User nuevoUsuario) {
         //encriptamos pw
 
@@ -23,5 +24,17 @@ public class UsuarioController {
         //cambiamos por la  pw encriptada
 
         daoUser.agregar(nuevoUsuario);
+    }
+    @RequestMapping(value="Usuario/hola", method= RequestMethod.GET)
+
+    public  Map<String, String> hola() {
+        Map<String, String> json= new HashMap<>();
+        json.put("Message","Hola");
+        //encriptamos pw
+
+
+        //cambiamos por la  pw encriptada
+        return json;
+
     }
 }
